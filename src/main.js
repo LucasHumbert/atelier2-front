b2abae8274fad9c7e0e39edd9860c1c0b592f76c
Calvin Lambert <calvin.lambert@protonmail.com>
2022-03-21 10:25:48 +0100

    :arrow_up: Upgrade package + setup for vue project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
@@ -10,3 +14,6 @@ new Vue({
   store,
   render: function (h) { return h(App) }
 }).$mount('#app')
+
+Vue.use(Buefy)
+Vue.use(VueAxios, axios)
