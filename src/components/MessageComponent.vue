fd6c8ed60a9d8e43d2f9877355ec7ce6d83350e3
Lucas Humbert <lucas.humbert.28@gmail.com>
2022-03-26 17:51:09 +0100

    :sparkles: commentaires

---
+++ src/components/MessageComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <span class="has-text-weight-bold">{{ message.user.firstname + " " +  message.user.lastname}}</span>
+     :
+    {{ message.content }}
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MessageComponent",
+  props: ['message']
+}
+</script>
+
+<style scoped>
+
+</style>
